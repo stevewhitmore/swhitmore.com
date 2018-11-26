5552d28ecbaac22949505169e82a2570dce93146
swhitmore <steve@swhitmore.com>
2018-11-26 11:52:29 -0600

    it's been vue-ified

---
+++ js/project-component.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#portfolio-inner',    
+    data: {
+        showSingle: false,
+        projects: [],
+        singleProject: {}
+    },
+    methods: {
+        displaySingleProject: function(event) {
+            event.preventDefault();
+            
+            this.singleProject = this.projects.find(p => p.id === event.target.id);                  
+            this.toggleSingleProject();
+        },
+        toggleSingleProject: function() {
+            this.showSingle = !this.showSingle;
+        }
+        
+    },
+    mounted () {
+        $.getJSON('./_in-memory-db/projects.json', data => {
+            this.projects = data;
+        })
+    }
+
+})
